88fa391b33b1aeec242308094041abe5e5fb7ed5
hareesh1807 <hareesh.jntuh@gmail.com>
2026-05-05 15:09:17 +0200

    final changes and updates

--- application2.py
+++ application2.py
@@ -9,6 +9,14 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    Time= time.strftime("%H:%M:%S")
-    return Time+" Serving from "+h_name+" ("+IP_addres+")\n"
+    Time = time.strftime("%H:%M:%S")
+    return Time + " Serving from " + h_name + " (" + IP_addres + ")\n"
 
+# Ansible uses this to check if the app is alive!
+@app.route('/health')
+def health():
+    return "OK", 200
+
+# The engine that actually turns the server on
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
